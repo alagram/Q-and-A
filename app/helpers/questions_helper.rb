8f50c5ade0b136356c3b51a41e78b9df7d34e16b
Albert Agram <albert.agram@gmail.com>
2018-01-31 16:22:51 +0000

    implement markdown and emoji

---
+++ app/helpers/questions_helper.rb
@@ -0,0 +1,11 @@
+module QuestionsHelper
+  def to_markdown(content)
+    pipeline_context = { gfm: true, asset_root: "https://a248.e.akamai.net/assets.github.com/images/icons" }
+    pipeline = HTML::Pipeline.new [
+      HTML::Pipeline::MarkdownFilter,
+      HTML::Pipeline::EmojiFilter,
+      HTML::Pipeline::SanitizationFilter,
+    ], pipeline_context
+    pipeline.call(content)[:output].to_s.html_safe
+  end
+end
