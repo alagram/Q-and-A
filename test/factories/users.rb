94855cb86fef00ecb2bbfabb4399ab6cffc7d108
Albert Agram <albert.agram@gmail.com>
2018-02-16 11:43:31 +0000

    install devise

--- test/factories/users.rb
+++ test/factories/users.rb
@@ -2,5 +2,8 @@ FactoryBot.define do
   factory :user do
     first_name "MyString"
     last_name "MyString"
+    email { Faker::Internet.email }
+    uid '12345'
+    password { Devise.friendly_token }
   end
 end
