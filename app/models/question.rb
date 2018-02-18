33a514a44488abcde2fa189db7c63c990ceb6691
Albert Agram <albert.agram@gmail.com>
2018-02-18 09:16:24 +0000

    order all answers by created_at desc

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,7 +1,7 @@
 class Question < ApplicationRecord
   include Friendlyable
   belongs_to :user
-  has_many :answers, -> { order("created_at DESC") }
+  has_many :answers, -> { order("created_at DESC") }, dependent: :delete_all
 
   validates :title, :presence => true
   validates :body, :presence => true
