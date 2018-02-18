883120c85a567686485066866e956aa93029f0ea
Albert Agram <albert.agram@gmail.com>
2018-02-18 15:29:34 +0000

    tell capybara to use puma

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -78,4 +78,7 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  config.before(:each, puma: true) do
+    Capybara.server = :puma
+  end
 end
