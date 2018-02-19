9b30c2f591938895d272e136371281b893229b47
Albert Agram <albert.agram@gmail.com>
2018-02-19 13:31:29 +0000

    config for shoulda

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -82,3 +82,11 @@ RSpec.configure do |config|
     Capybara.server = :puma
   end
 end
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+
+    with.library :rails
+  end
+end
